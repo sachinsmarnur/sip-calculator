f84cb057b6dad431a26a690028b5c995314dbf11
Sachin S Marnur <sachin.marnur99@gmail.com>
2026-03-31 11:41:58 +0530

    Updated sitemap

---
+++ sitemap.xml.js
@@ -0,0 +1,14 @@
+export default function handler(req, res) {
+    res.setHeader("Content-Type", "application/xml");
+    res.setHeader("Cache-Control", "no-store");
+
+    res.status(200).send(`<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+      <loc>https://sipinvestment-calculator.vercel.app/</loc>
+      <lastmod>2026-03-29</lastmod>
+      <changefreq>weekly</changefreq>
+      <priority>1.0</priority>
+    </url>
+  </urlset>`);
+}
